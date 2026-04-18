cc9a916c3d54d2062a04a445b7541aa63c61bfa9
chirayukale2006-gif <chirayukale2006@gmail.com>
2026-04-19 01:49:05 +0530

    Update config.js

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -12,7 +12,5 @@ const config = {
   JWT_SECRET: process.env.JWT_SECRET,
 };
 
-console.log("MONGO_URI", process.env.MONGO_URI);
 export default config;
 
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5ZTNjZTc1NTU1ZDE0YjE3M2M2MGRlZSIsImlhdCI6MTc3NjUzNzQyNywiZXhwIjoxNzc2NjIzODI3fQ.S7HslyUCmnT5P-Yjut_lhTc5iPczR2D5ZlQ2_10yJoY
